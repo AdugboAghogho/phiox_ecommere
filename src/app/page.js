e9d8aa2bad686e31443ba6f38f71746b2fd8ae4c
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-15 12:31:05 +0100

    jhh

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,17 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      Page
+      {/* <HeroBanner heroBanner={bannerData.length && bannerData[0]}  /> */}
+      <div className="products-heading">
+        <h2>Best Seller Products</h2>
+        <p>speaker There are many variations passages</p>
+      </div>
+
+      {/* <div className="products-container">
+        {products?.map((product) => <Product key={product._id} product={product} />)}
+      </div> */}
+
+      {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
     </div>
   )
 }
