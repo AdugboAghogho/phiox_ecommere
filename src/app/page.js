957956a356e4caaba760846c022e32b8932476e1
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-17 08:34:50 +0100

    d

--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,19 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 export default function Home() {
   return (
     <div>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+      {/* <HeroBanner heroBanner= {bannerData.length && bannerData[0]}  /> */}
+      <HeroBanner  />
       <div className="products-heading">
         <h2>Best Seller Products</h2>
         <p>speaker There are many variations passages</p>
       </div>
 
-      <div className="products-container">
+      {/* <div className="products-container">
         {products?.map((product) => <Product key={product._id} product={product} />)}
-      </div>
+      </div> */}
 
-      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+      <FooterBanner  />
+      {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
     </div>
   );
 }
