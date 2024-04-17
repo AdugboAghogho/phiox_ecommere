238ee2d12924c9bef24249012540e68b36ac4965
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-17 11:03:38 +0100

    f

--- components/index.js
+++ components/index.js
@@ -5,3 +5,14 @@ export { default as Products } from './Products';
 export { default as HeroBanner } from './HeroBanner';
 export { default as FooterBanner } from './FooterBanner';
 export { default as Cart } from './Cart';
+
+export async function getServerSideProps() {
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query);
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+  
+    return {
+      props: { products, bannerData },
+    };
+  }
