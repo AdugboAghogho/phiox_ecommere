4f69077120af51df2e62eef6e2bd4854179626c8
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-21 19:59:23 +0100

    f

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <div className="navbar-container">
-      <Link href="">
+      <Link href="/">
         <Image 
           src={logo} 
           alt="logo" 
